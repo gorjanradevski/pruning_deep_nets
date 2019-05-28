b38e4e9455280c1549d456f86b6da945c373cfe5
gorjanradevski <gorjanradevski@gmail.com>
2019-05-29 01:15:13 +0200

    inference pipeline and pycatche hopefully removed everywhere

---
+++ src/inference_pipeline.py
@@ -0,0 +1,102 @@
+import tensorflow as tf
+import logging
+import os
+from tqdm import tqdm
+import argparse
+
+
+from train_inference_utils.loaders import TestLoader
+from train_inference_utils.evaluator import Evaluator
+from train_inference_utils.models import BasicModel
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = "2"
+tf.logging.set_verbosity(tf.logging.ERROR)
+
+
+def inference(batch_size, load_model_path):
+    """Performs inference on the test set of the MNIST dataset.
+
+    Args:
+        batch_size: The size of the batch to use.
+        load_model_path: From where to load the model
+
+    Returns:
+
+    """
+    # Prepare the data
+    mnist = tf.keras.datasets.mnist
+    (_, _), (x_test, y_test) = mnist.load_data()
+    logger.info("Data prepared...")
+
+    # Reset the default graph and set the random seed
+    tf.reset_default_graph()
+    # Fixing the random seed
+    tf.set_random_seed(42)
+
+    # Create the data loader
+    loader = TestLoader(x_test, y_test, batch_size)
+    features, labels = loader.get_next()
+    logger.info("Loader created...")
+
+    # Create the model
+    model = BasicModel(features, labels)
+    logger.info("Model created...")
+
+    # Create evaluator
+    test_evaluator = Evaluator()
+
+    with tf.Session() as sess:
+        # Intialize the graph and set the best loss
+        model.init(sess, load_model_path)
+
+        try:
+            with tqdm(total=len(y_test)) as pbar:
+                while True:
+                    loss, labels, predictions = sess.run(
+                        [model.loss, model.labels, model.predictions]
+                    )
+                    pbar.update(len(labels))
+                    test_evaluator.update_metrics(loss, labels, predictions)
+        except tf.errors.OutOfRangeError:
+            pass
+
+        test_evaluator.update_best_accuracy()
+
+        logger.info(
+            f"The models accuracy on the MNIST dataset is: "
+            f"{test_evaluator.best_accuracy}"
+        )
+
+
+def main():
+    # Without the main sentinel, the code would be executed even if the script were
+    # imported as a module.
+    args = parse_args()
+    inference(args.batch_size, args.load_model_path)
+
+
+def parse_args():
+    """Parse command line arguments.
+    Returns:
+        Arguments
+    """
+    parser = argparse.ArgumentParser(
+        description="Performs inference on the MNIST test" "set."
+    )
+    parser.add_argument(
+        "--batch_size", type=int, default=64, help="The size of the batch"
+    )
+    parser.add_argument(
+        "--load_model_path",
+        type=str,
+        default="models/best_model",
+        help="From where to load the model",
+    )
+
+    return parser.parse_args()
+
+
+if __name__ == "__main__":
+    main()
--- src/train_inference_utils/loaders.py
+++ src/train_inference_utils/loaders.py
@@ -1,10 +1,37 @@
 import tensorflow as tf
+from abc import ABC, abstractmethod
 
 from basic_utils.constants import width, height, num_labels, num_prefetch
 
 
-class TrainValLoader:
+class BaseLoader(ABC):
+    def __init__(self, batch_size: int):
+        self.batch_size = batch_size
+
+    @staticmethod
+    def parse_data(image_matrix: tf.Tensor, label: tf.Tensor):
+        """Parses the the data.
+
+        - Unrolls the image matrix to be [28 * 28] instead of [28, 28].
+        - Converts the image label to one hot.
+
+        Args:
+            image_matrix: The matrix representing the image.
+            label: The label of the image.
+
+        Returns:
+            The unrolled image and the one-hotted label.
+
+        """
+        return (
+            tf.cast(tf.reshape(image_matrix, [width * height]), tf.float32),
+            tf.one_hot(label, depth=num_labels),
+        )
+
+
+class TrainValLoader(BaseLoader):
     def __init__(self, x_train, y_train, x_val, y_val, batch_size: int):
+        super().__init__(batch_size)
         # Create the training dataset
         self.train_dataset = tf.data.Dataset.from_tensor_slices((x_train, y_train))
         # Shuffle after each epoch
@@ -15,7 +42,7 @@ class TrainValLoader:
         )
 
         # Batch the data
-        self.train_dataset = self.train_dataset.batch(batch_size)
+        self.train_dataset = self.train_dataset.batch(self.batch_size)
         # Prefetch the batches when training on GPU
         self.train_dataset = self.train_dataset.prefetch(num_prefetch)
 
@@ -39,25 +66,33 @@ class TrainValLoader:
         self.train_init = self.iterator.make_initializer(self.train_dataset)
         self.val_init = self.iterator.make_initializer(self.val_dataset)
 
-    @staticmethod
-    def parse_data(image_matrix: tf.Tensor, label: tf.Tensor):
-        """Parses the the data.
-
-        - Unrolls the image matrix to be [28 * 28] instead of [28, 28].
-        - Converts the image label to one hot.
-
-        Args:
-            image_matrix: The matrix representing the image.
-            label: The label of the image.
+    def get_next(self):
+        """Utility wrapper around the get next method for readability.
 
         Returns:
-            The unrolled image and the one-hotted label.
+            The images and labels.
 
         """
-        return (
-            tf.cast(tf.reshape(image_matrix, [width * height]), tf.float32),
-            tf.one_hot(label, depth=num_labels),
+        images, labels = self.iterator.get_next()
+
+        return images, labels
+
+
+class TestLoader(BaseLoader):
+    def __init__(self, x_test, y_test, batch_size):
+        super().__init__(batch_size)
+        # Create the validation dataset
+        self.test_dataset = tf.data.Dataset.from_tensor_slices((x_test, y_test))
+        # Prepare the data in the right format
+
+        self.test_dataset = self.test_dataset.map(
+            self.parse_data, num_parallel_calls=tf.data.experimental.AUTOTUNE
         )
+        # Batch the data
+        self.test_dataset = self.test_dataset.batch(self.batch_size)
+        # Prefetch the batches when training on GPU
+        self.test_dataset = self.test_dataset.prefetch(num_prefetch)
+        self.iterator = self.test_dataset.make_one_shot_iterator()
 
     def get_next(self):
         """Utility wrapper around the get next method for readability.
