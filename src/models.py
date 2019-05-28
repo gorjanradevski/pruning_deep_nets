19ef709a85de70672cb29d7daa4a43fc1dd2cec4
gorjanradevski <gorjanradevski@gmail.com>
2019-05-28 21:53:14 +0200

    fixed .gitignore, the two types of pruning are included, tested and documented

---
+++ src/models.py
@@ -0,0 +1,71 @@
+import tensorflow as tf
+
+
+class BasicModel:
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def weight_pruning(w: tf.Variable, k: float) -> tf.Tensor:
+        """Performs pruning on a weight matrix w in the following way:
+
+        - The euclidean norm of each column is computed.
+        - The indices of smallest k% columns based on their euclidean norms are
+        selected.
+        - All elements in the columns that have the matching indices are set to 0.
+
+        Args:
+            w: The weight matrix.
+            k: The percentage of columns that should be pruned from the matrix.
+
+        Returns:
+            The weight pruned weight matrix.
+
+        """
+        k = tf.cast(
+            tf.round(tf.cast(tf.shape(w)[1], tf.float32) * tf.constant(k)),
+            dtype=tf.int32,
+        )
+        norm = tf.norm(w, axis=0)
+        row_indices = tf.tile(tf.range(tf.shape(w)[0]), [k])
+        _, col_indices = tf.nn.top_k(tf.negative(norm), k, sorted=True, name=None)
+        col_indices = tf.reshape(
+            tf.tile(tf.reshape(col_indices, [-1, 1]), [1, tf.shape(w)[0]]), [-1]
+        )
+        indices = tf.stack([row_indices, col_indices], axis=1)
+
+        return tf.scatter_nd_update(
+            w, indices, tf.zeros(tf.shape(w)[0] * k, tf.float32)
+        )
+
+    @staticmethod
+    def unit_pruning(w: tf.Tensor, k: float) -> tf.Tensor:
+        """Performs pruning on a weight matrix w in the following way:
+
+        - The absolute value of all elements in the weight matrix are computed.
+        - The indices of the smallest k% elements based on their absolute values are
+        selected.
+        - All elements with the matching indices are set to 0.
+
+        Args:
+            w: The weight matrix.
+            k: The percentage of values (units) that should be pruned from the matrix.
+
+        Returns:
+            The unit pruned weight matrix.
+
+        """
+        k = tf.cast(
+            tf.round(tf.size(w, out_type=tf.float32) * tf.constant(k)), dtype=tf.int32
+        )
+        w_reshaped = tf.reshape(w, [-1])
+        _, indices = tf.nn.top_k(
+            tf.negative(tf.abs(w_reshaped)), k, sorted=True, name=None
+        )
+        mask = tf.scatter_nd_update(
+            tf.Variable(tf.ones_like(w_reshaped, dtype=tf.float32)),
+            tf.reshape(indices, [-1, 1]),
+            tf.zeros([k], tf.float32),
+        )
+
+        return tf.reshape(w_reshaped * mask, tf.shape(w))
---
+++ src/test_models.py
@@ -0,0 +1,69 @@
+import pytest
+import tensorflow as tf
+import numpy as np
+
+from models import BasicModel
+
+
+@pytest.fixture
+def simple_weight_matrix():
+    return tf.Variable(tf.constant([[6, 3, 9], [1, 5, 7], [4, 8, 2]], dtype=tf.float32))
+
+
+@pytest.fixture
+def unit_pruned_simple_weight_matrix():
+    return np.array([[6, 0, 9], [0, 5, 7], [4, 8, 0]], dtype=np.float32)
+
+
+@pytest.fixture
+def weight_pruned_simple_weight_matrix():
+    return np.array([[0, 3, 9], [0, 5, 7], [0, 8, 2]], dtype=np.float32)
+
+
+@pytest.fixture
+def complex_weight_matrix():
+    return tf.Variable(tf.random.uniform([10, 10], dtype=tf.float32))
+
+
+@pytest.fixture
+def k_unit_prune():
+    return 0.34
+
+
+@pytest.fixture
+def k_weight_prune():
+    return 0.3
+
+
+def test_basic_unit_prune(
+    simple_weight_matrix, k_unit_prune, unit_pruned_simple_weight_matrix
+):
+
+    pruned_w = BasicModel.unit_pruning(simple_weight_matrix, k_unit_prune)
+    with tf.Session() as sess:
+        sess.run(tf.global_variables_initializer())
+        np.testing.assert_equal(sess.run(pruned_w), unit_pruned_simple_weight_matrix)
+
+
+def test_basic_weight_prune(
+    simple_weight_matrix, k_weight_prune, weight_pruned_simple_weight_matrix
+):
+    pruned_w = BasicModel.weight_pruning(simple_weight_matrix, k_weight_prune)
+    with tf.Session() as sess:
+        sess.run(tf.global_variables_initializer())
+        np.testing.assert_equal(sess.run(pruned_w), weight_pruned_simple_weight_matrix)
+
+
+def test_complex_weight_prune(complex_weight_matrix, k_weight_prune):
+    pruned_w = BasicModel.weight_pruning(complex_weight_matrix, k_weight_prune)
+    summed = tf.reduce_sum(pruned_w, axis=0)
+    with tf.Session() as sess:
+        sess.run(tf.global_variables_initializer())
+        assert np.count_nonzero(sess.run(summed)) == 7
+
+
+def test_complex_unit_prune(complex_weight_matrix, k_unit_prune):
+    pruned_w = BasicModel.unit_pruning(complex_weight_matrix, k_unit_prune)
+    with tf.Session() as sess:
+        sess.run(tf.global_variables_initializer())
+        assert np.count_nonzero(sess.run(pruned_w)) == 66
